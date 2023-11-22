68db079e80bd8170cee6c86fa49fce0fac4750ae
Kevin Halstead <mrmichael423@icloud.com>
2023-11-22 14:03:58 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Client from './client';
 
-const fetchWeatherData = async (lat: any, lon: any): Promise<any> => {
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e507ab1b58139c0750cc3127632434b4`)
-  return res.json()
+const fetchWeatherData = async (latitude: any, longitude: any): Promise<any> => {
+  if(latitude && longitude) {
+    const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}`);
+    return res.json()
+
+  }
 }
 
 export default async function Home({searchParams}: {searchParams: any}) {
