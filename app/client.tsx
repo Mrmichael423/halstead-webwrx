0ebf1182191f4cee8ccf041414ea6671da9e387f
Kevin Halstead <mrmichael423@icloud.com>
2023-11-28 20:00:57 -0500

    update

--- app/client.tsx
+++ app/client.tsx
@@ -1,63 +1,85 @@
-'use client'
-import {useState, useEffect} from 'react'
-export default function Client({searchParams, data}: {searchParams: any, data:any}) {
-  const [forecast, setForecast] = useState<any>();   
+"use client";
+import { useState, useEffect } from "react";
+export default function Client({
+  searchParams,
+  data,
+}: {
+  searchParams: any;
+  data: any;
+}) {
+  const [forecast, setForecast] = useState<any>();
 
-
-  const dailyHighTemps = data.daily.temperature_2m_max
-  const dailyLowTemps = data.daily.temperature_2m_min
-  const days = data.daily.time;
+  const dailyHighTemps = data?.daily?.temperature_2m_max;
+  const dailyLowTemps = data?.daily?.temperature_2m_min;
+  const days = data?.daily?.time;
 
   const getDayOfTheWeek = (day: string) => {
     const dateObject = new Date(day);
     // The getDay() method returns the day of the week (0-6, where 0 is Sunday and 6 is Saturday)
     const dayOfWeek = dateObject.getDay() + 1;
     // You may want to convert the numeric representation of the day to a string
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return daysOfWeek[dayOfWeek] || "Sunday";
+  };
 
-    return daysOfWeek[dayOfWeek] || 'Sunday';
+  useEffect(() => {
+    if (days) {
+      const SevenDayForecast = days.map((value: string, index: number) => ({
+        days: value,
+        highTemp: dailyHighTemps[index],
+        lowTemp: dailyLowTemps[index],
+      }));
+      console.log(
+        "🚀 ~ file: client.tsx:29 ~ SevenDayForecast:",
+        JSON.stringify(SevenDayForecast)
+      );
 
-  }
+      setForecast(SevenDayForecast.slice(1));
+    }
 
-  useEffect(()=> {
-    const SevenDayForecast = days.map((value:string, index:number) => ({
-      days: value,
-      highTemp: dailyHighTemps[index],
-      lowTemp: dailyLowTemps[index]
-    }))
-    console.log('🚀 ~ file: client.tsx:29 ~ SevenDayForecast:', JSON.stringify(SevenDayForecast));
-  
-    setForecast(SevenDayForecast.slice(1))
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-
-
-
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section>
       <div>
-        <p>Current weather for {searchParams.city}, {searchParams.region}</p>
-        <p>Current Temp: {data.current.temperature_2m}</p>
-        <p>Humidity: {data.current.relative_humidity_2m}</p>
-        <p>Feels like: {data.current.apparent_temperature}</p>
-        <p>High Temp: {data.daily.temperature_2m_max[0]}</p>
-        <p>Low Temp: {data.daily.temperature_2m_min[0]}</p>
+        {data && (
+          <>
+            <p>
+              Current weather for {searchParams.city}, {searchParams.region}
+            </p>
+            <p>Current Temp: {data.current.temperature_2m}</p>
+            <p>Humidity: {data.current.relative_humidity_2m}</p>
+            <p>Feels like: {data.current.apparent_temperature}</p>
+            <p>High Temp: {data.daily.temperature_2m_max[0]}</p>
+            <p>Low Temp: {data.daily.temperature_2m_min[0]}</p>
+          </>
+        )}
       </div>
-      <div  className="flex min-h-screen flex-row items-center justify-between p-24">
-        {forecast?.map((element:{ days: string, highTemp:string, lowTemp:string}, i:number) => {
-          return (
-          <div key={i}>
-            <p>Day: {getDayOfTheWeek(element.days)}</p>
-            <p>High Temp: {element.highTemp}</p>
-            <p>Low Temp: {element.lowTemp}</p>
-          </div>
-
-          )
-        })}
+      <div className="flex min-h-screen flex-row items-center justify-between p-24">
+        {forecast?.map(
+          (
+            element: { days: string; highTemp: string; lowTemp: string },
+            i: number
+          ) => {
+            return (
+              <div key={i}>
+                <p>Day: {getDayOfTheWeek(element.days)}</p>
+                <p>High Temp: {element.highTemp}</p>
+                <p>Low Temp: {element.lowTemp}</p>
+              </div>
+            );
+          }
+        )}
       </div>
     </section>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import Client from './client';
+import { Suspense } from 'react';
 
-const fetchWeatherData = async (latitude: any, longitude: any): Promise<any> => {
-  // if(latitude && longitude) {
+const fetchWeatherData = async (latitude: string, longitude:  string): Promise<any> => {
+  if(latitude && longitude) {
 
-    const res = await fetch(`https://api.open-meteo.com/v1/gfs?latitude=27.9475&longitude=-82.4584&current=temperature_2m,relative_humidity_2m,apparent_temperature&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York`);
+    const res = await fetch(`https://api.open-meteo.com/v1/gfs?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York`);
     return res.json()
 
-  // }
+  }
 }
 
 export default async function Home({searchParams}: {searchParams: any}) {
   const data = await fetchWeatherData(searchParams.latitude, searchParams.longitude)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Client searchParams={searchParams} data={data}/>
+      {/* <Suspense fallback={<p>Loading feed...</p>}> */}
+        <Client searchParams={searchParams} data={data}/>
+      {/* </Suspense> */}
     </main>
   )
 }
